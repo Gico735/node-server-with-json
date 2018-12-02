89afc9d8298b62b37982c1fbaf0a06906a0680ef
Gico735 <gic0@bk.ru>
2018-12-02 21:01:27 +0300

    add upload file

--- index.mjs
+++ index.mjs
@@ -3,9 +3,10 @@
 import fs from 'fs'
 import http from 'http'
 import URL from 'url'
+import formidable from 'formidable'
+import data from './static/data'
 
-const data = JSON.parse(fs.readFileSync('./static/data.json', 'utf-8'))
-
+// const data = JSON.parse(fs.readFileSync('./static/data.json', 'utf-8'))
 
 http.createServer((request, response) => {
   const url = URL.parse(request.url)
@@ -14,7 +15,7 @@ http.createServer((request, response) => {
   }
 
   if (~url.pathname.indexOf('/json')) {
-    const query = new RegExp('^'+url.query,'gi')
+    const query = new RegExp('^' + url.query, 'gi')
     console.log(query)
     const filterData = data.filter(el => {
       if (~el.search(query)) return el
@@ -24,5 +25,17 @@ http.createServer((request, response) => {
     });
     response.end(JSON.stringify(filterData))
   }
-}).listen(8080)
 
+  if (~url.pathname.indexOf('/file') && request.method.toLowerCase() == 'post') {
+    console.log(request.headers)
+    var form = new formidable.IncomingForm();
+    form.parse(request, function (err, fields, files) {
+      var oldpath = files.file.path;
+      var newpath = './static/userData/' + files.file.name;
+      fs.rename(oldpath, newpath, function (err) {
+        if (err) throw err;
+        response.end(fs.readFileSync('./static/index.html'))
+      });
+    });
+  }
+}).listen(8080)
